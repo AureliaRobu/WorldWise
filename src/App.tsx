54e9ecc8c545d3baec1f2d3ae16f879289f88d08
Aurelia Robu <robuaurelia@gmail.com>
2023-08-10 19:36:14 +0300

    main pages and styles

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+import { Route } from 'react-router';
+import HomePage from './pages/Homepage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
